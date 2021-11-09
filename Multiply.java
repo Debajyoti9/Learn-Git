94537b6834f188eeb3adc6ac91bf0be0d23487ab
rtbdeb <debajyotishee99@gmail.com>
2021-11-08 21:03:47 -0800

    change in multiply.java

--- Multiply.java
+++ Multiply.java
@@ -4,6 +4,6 @@ public class Multiply {
     public static void main(String[] args) {
         int a = 10;
         int b = 15;
-        System.out.println(a*b);
+        System.out.println("The multiplication of two num is: "+a*b);
     }
 }
